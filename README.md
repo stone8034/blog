faeb7d1059f6b91cf2a311c62f5385d6d45a317b
Mr.shi <5401668+stone8034@user.noreply.gitee.com>
2023-12-08 15:14:16 +0800

    完善文档README.md

---
+++ README.md
@@ -0,0 +1,15 @@
+## 项目脚本
+
+- 启动项目
+  ```
+  npm run dev
+  ```
+- 重新构建文档
+  ```
+  npm run build
+  ```
+- 项目打包发布 
+```
+  npm run deploy
+```
+
--- src/.vuepress/config.ts
+++ src/.vuepress/config.ts
@@ -2,18 +2,17 @@ import { defineUserConfig } from "vuepress";
 import theme from "./theme.js";
 
 export default defineUserConfig({
-  base: "/blog/",
-
+  base: "/blog/", // 和git仓库名称相同     gitpage地址  stone8034.github.io/仓库名/   【stone8034.github.io/blog/】
   locales: {
+    "/zh/": {
+      lang: "zh-CN",
+      title: "小石的博客",
+      description: "小石的博客",
+    },
     "/": {
       lang: "en-US",
       title: "Blog Demo",
       description: "A blog demo for vuepress-theme-hope",
-    },
-    "/zh/": {
-      lang: "zh-CN",
-      title: "博客演示",
-      description: "vuepress-theme-hope 的博客演示",
     },
   },
 
